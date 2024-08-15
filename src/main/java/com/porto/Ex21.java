a503e19c891c4869349c0b2ac3f53659d1ff016d
Leo Porto <leonardoreisporto@gmail.com>
2024-08-15 07:41:03 -0300

    exercicios

---
+++ src/main/java/com/porto/Ex21.java
@@ -0,0 +1,13 @@
+package com.porto;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        String str = "O rato roeu a roupa do rei de Roma";
+        int qtdPalavras = str.split(" ").length;
+
+        System.out.println(qtdPalavras);
+    }
+}
+
+//21. **Contagem de palavras em uma string:** Escreva um programa
+// que conte o número de palavras em uma string.
