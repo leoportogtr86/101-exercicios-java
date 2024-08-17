1edc9264205c726f60f9d45759f7a922f734f711
Leo Porto <leonardoreisporto@gmail.com>
2024-08-17 14:24:21 -0300

    exercicios

---
+++ src/main/java/com/porto/Ex22.java
@@ -0,0 +1,13 @@
+package com.porto;
+
+public class Ex22 {
+    public static void main(String[] args) {
+        String string = "Uma string qualquer";
+        String string1 = string.replaceAll(" ", "-");
+
+        System.out.println(string1);
+    }
+}
+
+//22. **Substituição de caracteres:** Crie um programa que substitua todos
+// os espaços em branco de uma string por hífens.
---
+++ src/main/java/com/porto/Ex23.java
@@ -0,0 +1,12 @@
+package com.porto;
+
+public class Ex23 {
+    public static void main(String[] args) {
+        String numero = "1";
+        int i = Integer.parseInt(numero);
+
+        System.out.println(i);
+    }
+}
+
+//23. **Conversão de tipo:** Escreva um programa que converta uma string para um número inteiro.
